d5b11f2a988d543267a32da0e8b9ca8ad7fa76e1
Andrew Pavlov <pavlovandreew@gmail.com>
2026-08-18 23:38:14 +0300

    cddddd disk

--- server/priv/migrations/20260818145715-Init.sql
+++ server/priv/migrations/20260818145715-Init.sql
@@ -10,7 +10,14 @@ CREATE TABLE stats_samples (
 CREATE INDEX stats_samples_sampled_at_idx
    ON stats_samples (sampled_at);
 
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY,
+    ip_address inet NOT NULL,
+    login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 --- migration:down
 DROP TABLE stats_samples;
 DROP TABLE users;
+DROP INDEX stats_samples_sampled_at_idx;
 --- migration:end
